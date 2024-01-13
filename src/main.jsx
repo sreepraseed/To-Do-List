e31d0c81ed769807cb72ab49a611a72b51289b09
Sreepraseed S <sreepraseed18@gmail.com>
2024-01-13 14:14:17 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './components/store';  // Update the import path
 import App from './App';
-import './app.css'; 
+import './App.css'; 
 ReactDOM.render(
   <Provider store={store}>
     <App />
